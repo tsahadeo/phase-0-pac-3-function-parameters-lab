213b1ee9d88b531ac5ec6865dbef4fa07b28894c
Travis <tsahadeo@mac.com>
2021-09-19 18:05:41 -0400

    Actually Completed Assignment

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-function Introduction(name) {
-    console.log(`The value is are ${name}.`);
-  }
-  function introductionWithLanguage (name, language) {
-    console.log(`The two values are ${name} and ${language}.`);
-    return "Hi, my name is [name] and I am learning to program in [language]"
-  }
-  
-  function sayHelloTo(firstName = "User") {
-    console.log(`Hello, ${firstName}!`);
+function introduction (name) {
+    return (`Hi, my name is ${name}.`)
   }
 
-  function introductionWithLanguageOptional (name, language) {
-    console.log(`The two values are ${name} and ${JavaScript}.`);
-    return "Hi, my name is [name] and I am learning to program in [language]"
+  function introductionWithLanguage (name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+  }
+
+  function introductionWithLanguageOptional (name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
   }
